631e5a1470d23bef8bad3c691800ae364ff90843
HeinNS <heinnaingsoe1999@gmail.com>
2024-09-11 11:41:39 +0700

    Add Data Grid with Material UI

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,11 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true
   },
-});
+  order: {
+    type: Number,
+    default: 0
+  }
+}, { strict: false });
 
 const Category = mongoose.models.category || mongoose.model("category", categorySchema);
 
